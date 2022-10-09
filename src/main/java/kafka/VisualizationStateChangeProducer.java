39cdeb86f14197f556b930ab183d88bde6d4c7cb
nwolniak <niedzwiedz107@gmail.com>
2022-10-09 21:35:15 +0200

    Added visualization and simulation state changes producers

---
+++ src/main/java/kafka/VisualizationStateChangeProducer.java
@@ -0,0 +1,43 @@
+package kafka;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Service;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
+import proto.model.RUNNING_STATE;
+import proto.model.VisualizationStateChangeMessage;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class VisualizationStateChangeProducer {
+
+    private final KafkaTemplate<String, VisualizationStateChangeMessage> simulationKafkaTemplate;
+
+    public void sendStateChangeMessage(RUNNING_STATE running_state) {
+        VisualizationStateChangeMessage visualizationStateChangeMessage = VisualizationStateChangeMessage.newBuilder()
+                .setStateChange(running_state)
+                .build();
+
+        var record = new ProducerRecord<String, VisualizationStateChangeMessage>(TopicConfiguration.VISUALIZATION_STATE_CHANGE_TOPIC,
+                visualizationStateChangeMessage);
+        ListenableFuture<SendResult<String, VisualizationStateChangeMessage>> future = simulationKafkaTemplate.send(record);
+
+        future.addCallback(new ListenableFutureCallback<>() {
+            @Override
+            public void onSuccess(SendResult<String, VisualizationStateChangeMessage> result) {
+                log.info("VisualizationStateChangeMessage send: {}", visualizationStateChangeMessage.getStateChange());
+            }
+
+            @Override
+            public void onFailure(@NotNull Throwable ex) {
+                log.info("Error while sending message: {}", visualizationStateChangeMessage + " " + ex.getMessage());
+            }
+        });
+    }
+}
