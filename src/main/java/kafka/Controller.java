76d747e6043a1c3089ce7174904581b83e05d78d
nwolniak <niedzwiedz107@gmail.com>
2022-11-16 11:23:34 +0100

    Simulation new nodes sending

--- src/main/java/kafka/Controller.java
+++ src/main/java/kafka/Controller.java
@@ -13,44 +13,50 @@ public class Controller {
 
     private final SimulationStateChangeProducer simulationStateChangeProducer;
     private final VisualizationStateChangeProducer visualizationStateChangeProducer;
+    private final SimulationNewNodesProducer simulationNewNodesProducer;
 
     @PostMapping(value = "/simulation/start")
     public void sendStartSimulationStateChangeMessage() {
-        this.simulationStateChangeProducer.sendStateChangeMessage(RUNNING_STATE.STARTED);
+        simulationStateChangeProducer.sendStateChangeMessage(RUNNING_STATE.STARTED);
     }
 
     @PostMapping(value = "/simulation/resume")
     public void sendResumeSimulationStateChangeMessage() {
-        this.simulationStateChangeProducer.sendStateChangeMessage(RUNNING_STATE.RESUMED);
+        simulationStateChangeProducer.sendStateChangeMessage(RUNNING_STATE.RESUMED);
     }
 
     @PostMapping(value = "/simulation/stop")
     public void sendStopSimulationStateChangeMessage() {
-        this.simulationStateChangeProducer.sendStateChangeMessage(RUNNING_STATE.STOPPED);
+        simulationStateChangeProducer.sendStateChangeMessage(RUNNING_STATE.STOPPED);
     }
 
     @PostMapping(value = "/simulation/close")
     public void sendCloseSimulationStateChangeMessage() {
-        this.simulationStateChangeProducer.sendStateChangeMessage(RUNNING_STATE.CLOSED);
+        simulationStateChangeProducer.sendStateChangeMessage(RUNNING_STATE.CLOSED);
     }
 
     @PostMapping(value = "/visualization/start")
     public void sendStartVisualizationStateChangeMessage() {
-        this.visualizationStateChangeProducer.sendStateChangeMessage(RUNNING_STATE.STARTED);
+        visualizationStateChangeProducer.sendStateChangeMessage(RUNNING_STATE.STARTED);
     }
 
     @PostMapping(value = "/visualization/resume")
     public void sendResumeVisualizationStateChangeMessage() {
-        this.visualizationStateChangeProducer.sendStateChangeMessage(RUNNING_STATE.RESUMED);
+        visualizationStateChangeProducer.sendStateChangeMessage(RUNNING_STATE.RESUMED);
     }
 
     @PostMapping(value = "/visualization/stop")
     public void sendStopVisualizationStateChangeMessage() {
-        this.visualizationStateChangeProducer.sendStateChangeMessage(RUNNING_STATE.STOPPED);
+        visualizationStateChangeProducer.sendStateChangeMessage(RUNNING_STATE.STOPPED);
     }
 
     @PostMapping(value = "/visualization/close")
     public void sendCloseVisualizationStateChangeMessage() {
-        this.visualizationStateChangeProducer.sendStateChangeMessage(RUNNING_STATE.CLOSED);
+        visualizationStateChangeProducer.sendStateChangeMessage(RUNNING_STATE.CLOSED);
+    }
+
+    @PostMapping(value = "/simulation/new-nodes")
+    public void sendSimulationNewNodesTransferMessage() {
+        simulationNewNodesProducer.sendSimulationNotOsmNodesTransferMessage();
     }
 }
---
+++ src/main/java/kafka/SimulationNewNodesProducer.java
@@ -0,0 +1,59 @@
+package kafka;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Service;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
+import proto.model.Node;
+import proto.model.SimulationNewNodesTransferMessage;
+
+import javax.sound.midi.Patch;
+import java.util.List;
+
+import static kafka.TopicConfiguration.SIMULATION_NEW_NODES_TOPIC;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class SimulationNewNodesProducer {
+
+    private final KafkaTemplate<String, SimulationNewNodesTransferMessage> kafkaTemplate;
+
+    private static final List<Node> nodeList = List.of(
+            Node.newBuilder().setNodeId("1").setLongitude(30.0).setLatitude(60.0).build(),
+            Node.newBuilder().setNodeId("2").setLongitude(31.0).setLatitude(61.0).build(),
+            Node.newBuilder().setNodeId("3").setLongitude(32.0).setLatitude(62.0).build(),
+            Node.newBuilder().setNodeId("4").setLongitude(33.0).setLatitude(63.0).build(),
+            Node.newBuilder().setNodeId("5").setLongitude(34.0).setLatitude(64.0).build(),
+            Node.newBuilder().setNodeId("6").setLongitude(35.0).setLatitude(65.0).build()
+    );
+
+    public void sendSimulationNotOsmNodesTransferMessage() {
+        final SimulationNewNodesTransferMessage simulationNotOsmNodesTransferMessage =
+                SimulationNewNodesTransferMessage.newBuilder().addAllNodes(nodeList).build();
+
+        var record = new ProducerRecord<String, SimulationNewNodesTransferMessage>(SIMULATION_NEW_NODES_TOPIC,
+                simulationNotOsmNodesTransferMessage);
+
+        ListenableFuture<SendResult<String, SimulationNewNodesTransferMessage>> future = kafkaTemplate.send(record);
+
+        future.addCallback(new ListenableFutureCallback<>() {
+            @Override
+            public void onSuccess(SendResult<String, SimulationNewNodesTransferMessage> result) {
+                log.info("SimulationNewNodesTransferMessage send {} new nodes",
+                        simulationNotOsmNodesTransferMessage.getNodesCount());
+            }
+
+            @Override
+            public void onFailure(@NotNull Throwable ex) {
+                log.info("Error while sending message: {}", ex.getMessage());
+            }
+        });
+    }
+
+}
